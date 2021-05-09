46cd4d55f0361df86ee1ce3010deed76811624fb
alamin <alaminsarkar177@gmail.com>
2021-05-09 20:27:26 +0600

    restart massages added

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ cd
 cd cli-calculator
 chmod 777 cli-calculator.py
 echo "Terminal shortcut successfully created"
+echo "If not working this shortcut, Please Restart termux app"
 echo "Next time just type: "
 echo "cli-cal"
 echo
